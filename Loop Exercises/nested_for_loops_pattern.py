76f7ab2f5f9e5cb893cb1569a740c1fcebb467a9
Tim Tinkler <timtdevbusiness@outlook.com>
2024-01-20 15:11:07 -0800

    two nested for loops print a pattern

---
+++ Loop Exercises/nested_for_loops_pattern.py
@@ -0,0 +1,9 @@
+for i in range(5):
+    print('')
+    for j in range(i+1):
+        print("*", end=" ")
+
+for i in range(3, -1, -1):
+    print('')
+    for j in range(i+1):
+        print('*', end=" ")
