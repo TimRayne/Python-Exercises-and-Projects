ec524395521a7fbb3ca3449bed8b19c7a6b1cdd6
Tim Tinkler <timtdevbusiness@outlook.com>
2024-02-01 22:10:10 -0800

    Split list into chunks and reverse the chunks

---
+++ Python Data Structure Practice/slice_list.py
@@ -0,0 +1,16 @@
+sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+
+chunk_1 = sample_list[0:3]
+print("Chunk 1 is:", chunk_1)
+chunk_1_rev = chunk_1[::-1]
+print('Reversing it:', chunk_1_rev)
+
+chunk_2 = sample_list[3:6]
+print('Chunk 2 is:', chunk_2)
+chunk_2_rev = chunk_2[::-1]
+print('Reversing it:', chunk_2_rev)
+
+chunk_3 = sample_list[6:9]
+print('Chunk 3 is:', chunk_3)
+chunk_3_rev = chunk_3[::-1]
+print('Reversing it:', chunk_3_rev)
